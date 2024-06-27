e73b055a7449debd0fc2cab4271bb63f5f0f44db
cybermasher <dschmidt2020@gmail.com>
2024-06-27 14:18:04 -0400

    Update defense_mechanism.py

--- src/defense_mechanism.py
+++ src/defense_mechanism.py
@@ -6,7 +6,7 @@ from adversarial_attack import create_adversarial_pattern
 
 def adversarial_training():
     (x_train, y_train), (x_test, y_test) = load_mnist_data()
-    model = tf.keras.models.load_model('models/mnist_model.h5')
+    model = tf.keras.models.load_model('models/my_model.keras')
 
     perturbations = create_adversarial_pattern(model, x_train, y_train)
     epsilon = 0.1
@@ -16,7 +16,7 @@ def adversarial_training():
     y_train_combined = tf.concat([y_train, y_train], axis=0)
 
     model.fit(x_train_combined, y_train_combined, epochs=5)
-    model.save('models/adversarial_trained_model.h5')
+    model.save('models/my_model.keras')
 
 if __name__ == '__main__':
     adversarial_training()
