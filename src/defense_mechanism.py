419fa17cfc47eaf539b21c8794da4aeb0c5d209c
cybermasher <dschmidt2020@gmail.com>
2024-06-30 17:25:47 -0400

    Update defense_mechanism.py

--- src/defense_mechanism.py
+++ src/defense_mechanism.py
@@ -1,12 +1,18 @@
 # src/defense_mechanism.py
 
 import tensorflow as tf
-from data_loader import load_mnist_data
+from functools import lru_cache
 from adversarial_attack import create_adversarial_pattern
+
+@lru_cache(maxsize=1)
+def load_mnist_data():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    return (x_train, y_train), (x_test, y_test)
 
 def adversarial_training():
     (x_train, y_train), (x_test, y_test) = load_mnist_data()
-    model = tf.keras.models.load_model('models/my_model.keras')
+    model = tf.keras.models.load_model('models/mnist_model_optuna.h5')
 
     perturbations = create_adversarial_pattern(model, x_train, y_train)
     epsilon = 0.1
@@ -16,7 +22,7 @@ def adversarial_training():
     y_train_combined = tf.concat([y_train, y_train], axis=0)
 
     model.fit(x_train_combined, y_train_combined, epochs=5)
-    model.save('models/my_model.keras')
+    model.save('models/adversarial_trained_model.h5')
 
 if __name__ == '__main__':
     adversarial_training()
